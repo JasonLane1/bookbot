b71f6e87377afa1c802e02b6910cb3705c99ee85
JasonRTL <jasonrtlane@gmail.com>
2025-05-02 17:37:30 +1000

    1.3 Unique characters placed into a dictionary with iterating values based on repeated charcters

--- stats.py
+++ stats.py
@@ -13,5 +13,14 @@ def word_counter(raw_string):
 def character_counter(raw_string):
     my_dict = {}
     concat_raw = raw_string().replace(" ", "").replace("\n", "")
+    for char in concat_raw.lower():
+        if char in my_dict.keys():
+            my_dict[char] += 1
+        else:
+            my_dict[char] = 1
+    return my_dict
+
+
+
+
     
-    return concat_raw
